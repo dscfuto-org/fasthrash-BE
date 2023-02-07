a8bfc42692e4e515da0ac940cce4537334a883b0
starlingvibes <starlingroot@protonmail.com>
2023-02-07 17:45:39 +0100

    fix: implemented image upload API via Google Cloud Storage

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const authRouter = require('./auth');
 const userRouter = require('./alertRoutes');
+const imageUploadRouter = require('./upload');
 
 const app = express();
 
+app.use('/', imageUploadRouter);
 app.use('/auth/', authRouter);
 app.use('/alerts/', userRouter);
 
