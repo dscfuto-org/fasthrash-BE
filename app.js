9e116f7694401c7f8e6c2018e452b0e5eb1b428c
starlingvibes <starlingroot@protonmail.com>
2023-02-07 10:29:27 +0100

    merge branch Emma_branch into main

--- app.js
+++ app.js
@@ -17,12 +17,11 @@ mongoose
   .connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     //don't show the log when it is test
-    // if (process.env.NODE_ENV !== 'test') {
-    //   console.log('Connected to %s', MONGODB_URL);
-    //   console.log('App is running ... \n');
-    //   console.log('Press CTRL + C to stop the process. \n');
-    // }
-    console.log('MongoDB database connection established successfully!');
+    if (process.env.NODE_ENV !== 'test') {
+      console.log('Connected to %s', MONGODB_URL);
+      console.log('App is running ... \n');
+      console.log('Press CTRL + C to stop the process. \n');
+    }
   })
   .catch((err) => {
     console.error('App starting error:', err.message);
