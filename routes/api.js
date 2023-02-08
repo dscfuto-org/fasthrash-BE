fde9fce17f86714391936c6c7cf8798ce2d95e40
starlingvibes <starlingroot@protonmail.com>
2023-02-08 17:02:59 +0100

    chore: added collector and organization alerts

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const authRouter = require('./auth');
-const userRouter = require('./alertRoutes');
+const userAlertRouter = require('./alert-collector');
+const collectorAlertRouter = require('./alert-org');
 const imageUploadRouter = require('./upload');
 
 const app = express();
 
 app.use('/', imageUploadRouter);
 app.use('/auth/', authRouter);
-app.use('/alerts/', userRouter);
+app.use('/alerts/', userAlertRouter);
+app.use('/org/alerts/', collectorAlertRouter);
 
 module.exports = app;
