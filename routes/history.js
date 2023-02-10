df1cf91f5fbc76d18aeeec236c24e4819238530a
starlingvibes <starlingroot@protonmail.com>
2023-02-10 11:59:51 +0100

    small fixes

--- routes/history.js
+++ routes/history.js
@@ -1,14 +1,20 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const historyController = require("../controllers/historyController");
+const historyController = require('../controllers/HistoryController');
 
-router.get("/getHistory/:id/", historyController.getHistory);
-router.put("/updateHistory/:id/", historyController.updateHistory);
-router.get("/getUserHistory/:userId/", historyController.getUserHistory);
-router.post("/addUserHistory/:userId", historyController.addUserHistory);
-router.delete('/deleteUserHistory/:userId/:historyId', historyController.deleteUserHistory);
-router.get("/getOrgHistory/:orgId/", historyController.getOrgHistory);
-router.post("/addOrgHistory/:orgId", historyController.addOrgHistory);
-router.delete('/deleteOrgHistory/:orgId/:historyId', historyController.deleteOrgHistory);
+router.get('/getHistory/:id/', historyController.getHistory);
+router.put('/updateHistory/:id/', historyController.updateHistory);
+router.get('/getUserHistory/:userId/', historyController.getUserHistory);
+router.post('/addUserHistory/:userId', historyController.addUserHistory);
+router.delete(
+  '/deleteUserHistory/:userId/:historyId',
+  historyController.deleteUserHistory
+);
+router.get('/getOrgHistory/:orgId/', historyController.getOrgHistory);
+router.post('/addOrgHistory/:orgId', historyController.addOrgHistory);
+router.delete(
+  '/deleteOrgHistory/:orgId/:historyId',
+  historyController.deleteOrgHistory
+);
 
 module.exports = router;
