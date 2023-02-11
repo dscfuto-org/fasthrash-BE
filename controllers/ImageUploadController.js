1e1a87757c75e01292a9ad8aa93bab109bd32068
starlingvibes <starlingroot@protonmail.com>
2023-02-11 17:57:37 +0100

    fix: worked on proper response for image upload API

--- controllers/ImageUploadController.js
+++ controllers/ImageUploadController.js
@@ -7,6 +7,8 @@ const bucket = storage.bucket('fastrash-image-upload');
 const uuid = require('uuid');
 
 const upload = async (req, res) => {
+  const NON_IMAGE =
+    'Error: Invalid file type. Only jpg, jpeg, and png files are allowed.';
   try {
     await processFile(req, res);
 
@@ -15,7 +17,7 @@ const upload = async (req, res) => {
     }
 
     // Create a new blob in the bucket and upload the file data.
-    const uniquename = `${req.file.fieldname}-${uuid.v4()}-${
+    let uniquename = `${req.file.fieldname}-${uuid.v4()}-${
       req.file.originalname
     }`;
     const blob = bucket.file(uniquename);
@@ -38,27 +40,22 @@ const upload = async (req, res) => {
         // Make the file public
         await bucket.file(req.file.originalname).makePublic();
       } catch (err) {
-        return res.status(500).send({
-          message: `Uploaded the file successfully: ${req.file.originalname}, but public access is denied!`,
+        return res.status(200).send({
+          message: `Uploaded the file successfully: ${req.file.originalname}`,
           url: publicUrl,
         });
       }
-
-      res.status(200).send({
-        message: 'Uploaded the file successfully: ' + req.file.originalname,
-        url: publicUrl,
-      });
     });
 
     blobStream.end(req.file.buffer);
   } catch (err) {
-    if (err.code == 'LIMIT_FILE_SIZE') {
-      return res.status(500).json({
+    if (err.code === 'LIMIT_FILE_SIZE') {
+      return res.status(400).json({
         message: 'File cannot be larger than 10MB',
       });
     }
-    res.status(500).send({
-      message: `Could not upload the file: ${err}`,
+    res.status(err !== NON_IMAGE ? 400 : 500).send({
+      message: `Could not upload the file - ${err}`,
     });
   }
 };
