967e154a90d80fe5d85a06511be538fcf19381ad
starlingvibes <starlingroot@protonmail.com>
2023-02-11 16:39:33 +0100

    chore: added tests for user and org auth

--- test/auth.js
+++ test/auth.js
@@ -1,5 +1,6 @@
 const { chai, server } = require('./testConfig');
 const UserModel = require('../models/UserModel');
+const OrgModel = require('../models/OrgModel');
 /**
  * Test cases to test all the authentication APIs
  * Covered Routes:
@@ -7,7 +8,7 @@ const UserModel = require('../models/UserModel');
  * (2) Register
  */
 
-describe('Authentication', () => {
+describe('User authentication', () => {
   // Before each test we empty the database
   before((done) => {
     // eslint-disable-next-line no-unused-vars
@@ -16,8 +17,8 @@ describe('Authentication', () => {
     });
   });
 
-  // Prepare data for testing
-  const testData = {
+  // Prepare user data for testing
+  const userTestData = {
     firstName: 'Chidera',
     lastName: 'Anichebe',
     location: '12.123, 12.4',
@@ -28,32 +29,90 @@ describe('Authentication', () => {
   };
 
   /*
-   * Test the /POST route
+   * Test the user registration route
    */
   describe('POST /api/auth/register', () => {
     it('It should register a new user', (done) => {
       chai
         .request(server)
         .post('/api/auth/register')
-        .send(testData)
+        .send(userTestData)
         .end((err, res) => {
           res.should.have.status(201);
           res.body.should.have.property('message');
-          testData._id = res.body._id;
+          userTestData._id = res.body._id;
           done();
         });
     });
   });
 
   /*
-   * Test the /POST route
+   * Test the user login route
    */
-  describe('/POST Login', () => {
+  describe('POST /api/auth/login', () => {
     it('It should do a successful user login', (done) => {
       chai
         .request(server)
         .post('/api/auth/login')
-        .send({ email: testData.email, password: testData.password })
+        .send({ email: userTestData.email, password: userTestData.password })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have
+            .property('message')
+            .eql('Authorization successful');
+          done();
+        });
+    });
+  });
+});
+
+describe('Organization authentication', () => {
+  // Before each test we empty the database
+  before((done) => {
+    // eslint-disable-next-line no-unused-vars
+    OrgModel.deleteMany({}, (err) => {
+      done();
+    });
+  });
+
+  // Prepare organization data for testing
+  const orgTestData = {
+    businessName: 'Wall Street Cartel',
+    location: '12.123, 12.4',
+    size: 'Large',
+    yearsOfOperation: 10,
+    email: 'root@localhost.com',
+    phoneNumber: '08123456789',
+    password: 'sup3rs3cur3pwd',
+  };
+
+  /*
+   * Test the organization registration route
+   */
+  describe('POST /api/auth/org/register', () => {
+    it('It should register a new organization account', (done) => {
+      chai
+        .request(server)
+        .post('/api/auth/org/register')
+        .send(orgTestData)
+        .end((err, res) => {
+          res.should.have.status(201);
+          res.body.should.have.property('message');
+          orgTestData._id = res.body._id;
+          done();
+        });
+    });
+  });
+
+  /*
+   * Test the organization login route
+   */
+  describe('POST /api/auth/org/login', () => {
+    it('It should do a successful organization login', (done) => {
+      chai
+        .request(server)
+        .post('/api/auth/org/login')
+        .send({ email: orgTestData.email, password: orgTestData.password })
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.have
