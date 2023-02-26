90d71aa6e40e228a26755d429069ead313641c84
starlingvibes <starlingroot@protonmail.com>
2023-02-26 18:37:22 +0100

    docs: updated forgot password and history documentation

--- routes/history.js
+++ routes/history.js
@@ -7,34 +7,61 @@ const collectionHistoryController = require('../controllers/historyControllers/C
 /*
   create, fetch, update & delete user deposit history
 */
-router.get('/depositHistory/fetch/:id/', depositHistoryController.getDepositHistory);
-router.get('/depositHistory/fetchAll/:userId/', depositHistoryController.getAllDepositHistory);
-router.post('/depositHistory/create/:userId', depositHistoryController.addDepositHistory);
-router.put('/depositHistory/update/:id/', depositHistoryController.updateDepositHistory);
+router.get('/deposits/fetch/:id/', depositHistoryController.getDepositHistory);
+router.get(
+  '/deposits/user/:userId/',
+  depositHistoryController.getAllDepositHistory
+);
+router.post(
+  '/deposits/create/:userId',
+  depositHistoryController.addDepositHistory
+);
+router.put(
+  '/deposits/update/:id/',
+  depositHistoryController.updateDepositHistory
+);
 router.delete(
-  '/depositHistory/:userId/:historyId',
+  '/deposits/:userId/:historyId',
   depositHistoryController.deleteDepositHistory
 );
 // history/collectionHistory
 /*
   create, fetch, update & delete user collection history
 */
-router.get('/collectionHistory/fetch/:id', collectionHistoryController.getCollectionHistory);
-router.get('/collectionHistory/fetchAll/:userId', collectionHistoryController.getCollectorCollectionHistory);
-router.post('/collectionHistory/create/:userId', collectionHistoryController.addCollectorCollectionHistory);
-router.put('/collectionHistory/update/:id', collectionHistoryController.updateCollectionHistory);
+router.get(
+  '/collections/fetch/:id',
+  collectionHistoryController.getCollectionHistory
+);
+router.get(
+  '/collections/collector/:userId',
+  collectionHistoryController.getCollectorCollectionHistory
+);
+router.post(
+  '/collections/create/:userId',
+  collectionHistoryController.addCollectorCollectionHistory
+);
+router.put(
+  '/collections/update/:id',
+  collectionHistoryController.updateCollectionHistory
+);
 router.delete(
-  '/collectionHistory/:userId/:historyId',
+  '/collections/:userId/:historyId',
   collectionHistoryController.deleteCollectorCollectionHistory
 );
 // history/organization
 /*
   create, fetch, update & delete user deposit history
 */
-router.get('/orgHistory/fetch/:orgId', collectionHistoryController.getOrgCollectionHistory);
-router.post('/orgHistory/create/:orgId', collectionHistoryController.addOrgCollectionHistory);
+router.get(
+  '/org/fetch/:orgId',
+  collectionHistoryController.getOrgCollectionHistory
+);
+router.post(
+  '/org/create/:orgId',
+  collectionHistoryController.addOrgCollectionHistory
+);
 router.delete(
-  '/orgHistory/:orgId/:historyId',
+  '/org/:orgId/:historyId',
   collectionHistoryController.deleteOrgCollectionHistory
 );
 
