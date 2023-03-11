487edbe48fc8fa65c0cd576a5874ec7f11e19ea8
Oputa Olivia Amarachi <72948572+oputaolivia@users.noreply.github.com>
2023-03-11 16:32:43 +0000

    routes protected

--- routes/alert-collector.js
+++ routes/alert-collector.js
@@ -6,7 +6,7 @@ const upload = multer();
 const { verifyUser } = require('../middlewares/verifyUser');
 
 router.get('/', alertController.getAllAlertsByRole);
-router.post('/create/', upload.single('file'),verifyUser, alertController.createAlert);
+router.post('/create/',verifyUser, upload.single('file'), alertController.createAlert);
 router.put('/update/:id', verifyUser, alertController.updateAlertStatus);
 router.get('/:id/', verifyUser, alertController.getAlert);
 router.delete('/delete/:id/', verifyUser, alertController.deleteAlert);
