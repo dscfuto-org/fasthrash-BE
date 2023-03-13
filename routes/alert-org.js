76527f7288f14b47c2a85c966f1254688c7dc35a
starlingvibes <starlingroot@protonmail.com>
2023-03-13 18:14:52 +0100

    fix: fixed authorization bug

--- routes/alert-org.js
+++ routes/alert-org.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const alertController = require('../controllers/AlertController');
-const { verifyUser } = require('../middlewares/verifyUser');
+const { verifyOrg } = require('../middlewares/verifyUser');
 const processFileMiddleware = require('../middlewares/upload');
 const { visionAIFilter } = require('../middlewares/vision');
 
 router.get('/', alertController.getAllAlertsByRole);
 router.post(
   '/create/',
-  verifyUser,
+  verifyOrg,
   processFileMiddleware,
   visionAIFilter,
   alertController.createAlert
 );
-router.put('/update/:id', verifyUser, alertController.updateAlertStatus);
-router.get('/:id/', verifyUser, alertController.getAlert);
-router.delete('/delete/:id/', verifyUser, alertController.deleteAlert);
+router.put('/update/:id', verifyOrg, alertController.updateAlertStatus);
+router.get('/:id/', verifyOrg, alertController.getAlert);
+router.delete('/delete/:id/', verifyOrg, alertController.deleteAlert);
 
 module.exports = router;
