066741e9791f8e10f153a45c221a1468af37a2a3
starlingvibes <starlingroot@protonmail.com>
2023-03-22 15:24:46 +0100

    chore: removed console log

--- helpers/cloudStorage.js
+++ helpers/cloudStorage.js
@@ -7,41 +7,42 @@ const storage = new Storage({ keyFilename: 'storage-keys.json' });
 const bucket = storage.bucket('fastrash-image-upload');
 
 /**
- * 
- * @param {import('express').Request} req 
- * @param {import('express').Response} res 
+ *
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
  */
 const cloudUpload = async (req) => {
-    if (req.files.length === 0) throw new Error('Please upload at least 1 file!');
-    const alertImages = [];
-    const uploadErrors = [];
+  if (req.files.length === 0) throw new Error('Please upload at least 1 file!');
+  const alertImages = [];
+  const uploadErrors = [];
 
-    for (const file of req.files) {
-        try {
-            // create a new blob in the bucket and upload the file data
-            let uniquename = `${file.fieldname}-${uuid.v4()}-${file.originalname}`;
-            // const { apiResponse } = await bucket.upload(file.originalname, { resumable: false, destination: uniquename });
-            // console.log(apiResponse);
+  for (const file of req.files) {
+    try {
+      // create a new blob in the bucket and upload the file data
+      let uniquename = `${file.fieldname}-${uuid.v4()}-${file.originalname}`;
+      // const { apiResponse } = await bucket.upload(file.originalname, { resumable: false, destination: uniquename });
+      // console.log(apiResponse);
 
-            const blob = bucket.file(uniquename);
-            await blob.save(file.buffer, {resumable: false});
-            await blob.makePublic();
-            const publicUrl = format(`https://storage.googleapis.com/${bucket.name}/${blob.name}`);
+      const blob = bucket.file(uniquename);
+      await blob.save(file.buffer, { resumable: false });
+      await blob.makePublic();
+      const publicUrl = format(
+        `https://storage.googleapis.com/${bucket.name}/${blob.name}`
+      );
 
-            const newImage = new imageSchema({ url: publicUrl });
-            await newImage.save();
+      const newImage = new imageSchema({ url: publicUrl });
+      await newImage.save();
 
-            alertImages.push(publicUrl);
-        } catch (error) {
-            console.log(error);
-            uploadErrors.push(error.message);
-            continue;
-        }
+      alertImages.push(publicUrl);
+    } catch (error) {
+      uploadErrors.push(error.message);
+      continue;
     }
+  }
 
-    return { alertImages, uploadErrors };
+  return { alertImages, uploadErrors };
 };
 
 module.exports = {
-    cloudUpload,
+  cloudUpload,
 };
